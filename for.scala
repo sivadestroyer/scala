64358f05110065f1c26263957a61071ad7775bf3
SIVABALAMURUGAN M <sbmsakthi007@gmail.com>
2023-05-21 11:45:36 +0530

    for loop

---
+++ for.scala
@@ -0,0 +1,20 @@
+object whiledemo
+{
+    def main(arge:Array[String])
+    {
+        var i = 4
+        var x=1
+       
+       for (y <- 1 to i )
+       {
+           println(i)
+       }
+    }
+}
+
+//output
+/*
+4
+4
+4
+4*/

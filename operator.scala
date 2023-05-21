34d333c025bf50285d329717f00fad7609181396
SIVABALAMURUGAN M <sbmsakthi007@gmail.com>
2023-05-21 10:31:48 +0530

    operators in scala

---
+++ operator.scala
@@ -0,0 +1,9 @@
+object operrator{
+    def main(args:Array[String])
+    {
+         var a=3; // mutable variable
+         val b=4; // immutable variable because val is used 
+         println("a+b:"+(a+b);//it gives a+b:7
+
+    }
+}

6a4080057935da22c99f3ab730eeb9b07b92d20c
SIVABALAMURUGAN M <sbmsakthi007@gmail.com>
2023-05-21 20:55:50 +0530

    array

---
+++ finally.scala
@@ -0,0 +1,13 @@
+object finally{
+    def main(args: Array[String])
+    {
+        var arr =new Array[String](3)
+        arr(0)= "siva"
+        arr(1)="bala"
+        arr(2)="murugan"
+        for (i <- 0 to 2)
+        print(arr(i))
+    }
+}
+/* output
+sivabalamurugan */

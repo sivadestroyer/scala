5641531963abff79a00692292c7ee3dab17bddca
SIVABALAMURUGAN M <sbmsakthi007@gmail.com>
2023-05-21 11:23:45 +0530

    fact

---
+++ factorial.scala
@@ -0,0 +1,10 @@
+object factorial
+{
+    def main(args: Array[String])
+    {
+        println(factorial(30));
+    }
+    def factorial(x:BigInt): BigInt =
+        if (x==0) 1 else x*factorial(x-1)
+        
+}
---
+++ while.scala
@@ -0,0 +1,13 @@
+object whiledemo
+{
+    def main(arge:Array[String])
+    {
+        var i = 4
+        var x=1
+        while (x<=4)
+        {
+            println(i)
+            x+=1
+        }
+    }
+}

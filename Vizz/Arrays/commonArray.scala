1f75143bbee7a79ddd9c771088e8f8fbecc4a2e8
vinayak5002 <mailvizzard@gmail.com>
2023-07-02 16:47:32 +0530

    updated commonArray to not use inbuilt functions

--- Vizz/Arrays/commonArray.scala
+++ Vizz/Arrays/commonArray.scala
@@ -2,12 +2,28 @@
 
 object Ground {
 
+    def containsInArray(e: Int, arr: Array[Int]): Boolean = {
+        var ret: Boolean = false
+
+        for( i <- arr ) {
+            if( e == i ) {
+                ret = true
+                ret
+            }
+        }
+
+        ret
+    }
+
     // common elemenrs in 2 lists
     def foo(arr1: Array[Int], arr2: Array[Int]): Array[Int] = {
         var ret: Array[Int] = Array.empty[Int]
 
         for ( i <- arr2 ) {
-            if( arr1.contains(i) ) {
+            // if( arr1.contains(i) ) {
+            //     ret = ret :+ i
+            // }
+            if( containsInArray(i, arr1) ) {
                 ret = ret :+ i
             }
         }
